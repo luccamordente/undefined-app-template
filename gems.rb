58cfda76c056717d682e5907d7d0061eed371607
Heitor Salazar <heitorsalazar@gmail.com>
2012-12-03 19:02:12 -0200

    add some more gems

--- gems.rb
+++ gems.rb
@@ -14,5 +14,21 @@ end
 
 # liste as gems aqui
 
-GEMS["simple_form"] = '~> 2.0.2'
-GEMS["fabrication"] = '~> 2.5'
+# App (Development & Production)
+GEMS["simple_form"] = '>= 2.0.2'
+
+# Test
+GEMS["fabrication"]      = '>= 2.5'
+GEMS["database_cleaner"] = '>= 0.9.1'
+GEMS["forgery"]          = '>= 0.5.0'
+
+GEMS["rspec"]            = '>= 2.11'
+GEMS["rspec-rails"]      = '>= 2.11'
+
+GEMS["guard"]            = '>= 1.5'
+GEMS["guard-rspec"]      = '>= 2.1'
+GEMS["rb-fsevent"]       = '>= 0.9.2'
+
+# Deploy
+GEMS["capistrano"]        = '>= 1.5'
+GEMS["capistrano_colors"] = '>= 1.5'
